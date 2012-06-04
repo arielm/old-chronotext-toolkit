f7e0dfd529ec04cc47eb9cb4b291f0ed4b5e054a
arielm <ariel@chronotext.org>
2012-06-04 11:06:27 +0300

    TURNING TABS INTO SPACE

--- Toolkit/XFont/XFontSequence.h
+++ Toolkit/XFont/XFontSequence.h
@@ -4,30 +4,30 @@
 
 typedef struct Slot
 {
-	int count;
-	GLfloat *vertice;
-	GLfloat *coords;
-	struct Slot *next;
+    int count;
+    GLfloat *vertice;
+    GLfloat *coords;
+    struct Slot *next;
 } Slot;
 
 class XFont;
 
 class XFontSequence
 {
-	XFont *font;
+    XFont *font;
     int dimensions;
     Slot *first, *last, *current;
     int ended;
-    
+
     void dispose();
 
 public:
-	XFontSequence();
-	~XFontSequence();
-	
-	void begin(XFont *font, int dimensions);
-	void end();
-	void flush(GLfloat *vertice, GLfloat *coords, int count);
-	void replay();
+    XFontSequence();
+    ~XFontSequence();
+
+    void begin(XFont *font, int dimensions);
+    void end();
+    void flush(GLfloat *vertice, GLfloat *coords, int count);
+    void replay();
     void clear();
 };
