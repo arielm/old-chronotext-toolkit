aabbc70a9df7889076128a02d5182cf6a38c18e2
arielm <ariel@chronotext.org>
2012-06-14 15:49:52 +0300

    FIXING "BAD COPY/PASTE" DEFECT

--- Toolkit/Utils/Utils.cpp
+++ Toolkit/Utils/Utils.cpp
@@ -18,7 +18,7 @@ string wstringToUtf8(const wstring &s)
 	
 	vector<char> resultString( utf8Size );
 	
-	int convResult = ::/Users/arielm/Documents/DEV/chronotext-public/Toolkit/TextWideCharToMultiByte( CP_UTF8, 0, s.c_str(), -1, &resultString[0], utf8Size, NULL, NULL );
+	int convResult = ::WideCharToMultiByte( CP_UTF8, 0, s.c_str(), -1, &resultString[0], utf8Size, NULL, NULL );
 	
 	if( convResult != utf8Size )
 	{
