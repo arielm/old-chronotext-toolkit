2570de6b204f2f5199289a0a0f5d436af6173ed0
arielm <ariel@chronotext.org>
2012-06-18 15:03:28 +0300

    TextureAtlasAnimation: CAPABILITY TO DEFINE SUB-ANIMATIONS AND FRAME-RATE

--- Toolkit/Texture/TextureAtlasAnimation.cpp
+++ Toolkit/Texture/TextureAtlasAnimation.cpp
@@ -1,13 +1,50 @@
 #include "TextureAtlasAnimation.h"
 
-TextureAtlasAnimation::TextureAtlasAnimation(TextureAtlas *atlas, const std::string &path) : atlas(atlas)
+using namespace std;
+
+TextureAtlasAnimation::TextureAtlasAnimation(TextureAtlas *atlas, const string &path) : fps(24)
 {
     sprites = atlas->getAnimationSprites(path);
+}
+
+TextureAtlasAnimation::TextureAtlasAnimation(TextureAtlas *atlas, const string &path, float fps) : fps(fps)
+{
+    sprites = atlas->getAnimationSprites(path);
+}
+
+TextureAtlasAnimation::TextureAtlasAnimation(TextureAtlas *atlas, const string &path, int firstFrameIndex, int lastFrameIndex, float fps) : fps(fps)
+{
+    vector<Sprite*> tmp = atlas->getAnimationSprites(path);
+    
+    if (((firstFrameIndex < 0) || (firstFrameIndex >= tmp.size()) || ((lastFrameIndex <0) || (lastFrameIndex >= tmp.size()))))
+    {
+        throw std::runtime_error ("OUT-OF-RANGE ANIMATION FRAMES");
+    }
+    
+    if (firstFrameIndex < lastFrameIndex)
+    {
+        for (int i = firstFrameIndex; i <= lastFrameIndex; i++)
+        {
+            sprites.push_back(tmp[i]);
+        }
+    }
+    else
+    {
+        for (int i = firstFrameIndex; i >= lastFrameIndex; i--)
+        {
+            sprites.push_back(tmp[i]);
+        }
+    }
 }
 
 int TextureAtlasAnimation::getFrameCount()
 {
     return sprites.size();
+}
+
+float TextureAtlasAnimation::getDuration()
+{
+    return sprites.size() / fps;
 }
 
 void TextureAtlasAnimation::drawFromCenter(int frameIndex, bool originUp)
@@ -22,6 +59,12 @@ void TextureAtlasAnimation::drawFromCenter(float t, bool originUp)
     drawFromCenter(frameIndex, originUp);
 }
 
+void TextureAtlasAnimation::playFromCenter(double now, bool originUp)
+{
+    float t = fmodf(now / getDuration(), 1);
+    drawFromCenter(t, originUp);
+}
+
 void TextureAtlasAnimation::draw(int frameIndex, float rx, float ry, bool originUp)
 {
     Sprite *sprite = sprites[frameIndex % sprites.size()];
@@ -32,4 +75,10 @@ void TextureAtlasAnimation::draw(float t, float rx, float ry, bool originUp)
 {
     int frameIndex = t * sprites.size();
     draw(frameIndex, originUp);
+}
+
+void TextureAtlasAnimation::play(double now, float rx, float ry, bool originUp)
+{
+    float t = fmodf(now / getDuration(), 1);
+    draw(t, rx, ry, originUp);
 }
--- Toolkit/Texture/TextureAtlasAnimation.h
+++ Toolkit/Texture/TextureAtlasAnimation.h
@@ -6,17 +6,22 @@
 
 class TextureAtlasAnimation
 {
-    TextureAtlas *atlas;
     std::vector<Sprite*> sprites;
+    float fps;
 
 public:
     TextureAtlasAnimation(TextureAtlas *atlas, const std::string &path);
+    TextureAtlasAnimation(TextureAtlas *atlas, const std::string &path, float fps);
+    TextureAtlasAnimation(TextureAtlas *atlas, const std::string &path, int firstFrameIndex, int lastFrameIndex, float fps);
     
     int getFrameCount();
+    float getDuration();
 
     void drawFromCenter(int frameIndex, bool originUp = true);
     void drawFromCenter(float t, bool originUp = true);
+    void playFromCenter(double now, bool originUp = true);
     
     void draw(int frameIndex, float rx = 0, float ry = 0, bool originUp = true);
     void draw(float t, float rx = 0, float ry = 0, bool originUp = true);
+    void play(double now, float rx = 0, float ry = 0, bool originUp = true);
 };
