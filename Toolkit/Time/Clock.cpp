b92359be76ded26f2553c1a7a538fd79be8e8e40
arielm <ariel@chronotext.org>
2012-06-18 17:06:26 +0300

    BASIC Clock STUFF IN PLACE

---
+++ Toolkit/Time/Clock.cpp
@@ -0,0 +1,9 @@
+//
+//  Clock.cpp
+//  TimeBasedAnimation1_OSX
+//
+//  Created by Ariel Malka on 6/18/12.
+//  Copyright (c) 2012 chronotext.com. All rights reserved.
+//
+
+#include "Clock.h"
---
+++ Toolkit/Time/TimeBase.h
@@ -0,0 +1,9 @@
+//
+//  TimeBase.h
+//  TimeBasedAnimation1_OSX
+//
+//  Created by Ariel Malka on 6/18/12.
+//  Copyright (c) 2012 chronotext.com. All rights reserved.
+//
+
+#pragma once
