ac806b6369f519d1089a76d349ffc73397d8f726
arielm <ariel@chronotext.org>
2012-06-23 02:01:41 +0300

    LinkExtractor IN PLACE

---
+++ Toolkit/Text/LinkExtractor.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <string>
+
+struct ExtractedLink
+{
+    int offset;
+    int length;
+    std::string url;
+    
+    ExtractedLink(int offset, int length, std::string url) : offset(offset), length(length), url(url) {}
+};
+
+class LinkExtractor
+{
+public:
+    static std::pair<std::vector<ExtractedLink>, std::string> extract(const std::string &input)
+    {
+        std::vector<ExtractedLink> links;
+        std::string output;
+        
+        size_t pos = 0;
+        
+        while (true)
+        {
+            int pos1 = input.find("<a href=\"", pos);
+            
+            if (pos1 != std::string::npos)
+            {
+                int pos2 = pos1 + 9;
+                int pos3 = input.find("\">", pos2);
+                
+                int pos4 = pos3 + 2;
+                int pos5 = input.find("</a>", pos4);
+
+                links.push_back(ExtractedLink(pos4, pos5 - pos4, input.substr(pos2, pos3 - pos2)));
+
+                output.append(input, pos, pos1 - pos);
+                output.append(input, pos4, pos5 - pos4);
+
+                pos = pos5 + 4;
+            }
+            else
+            {
+                output.append(input.begin() + pos, input.end());
+                break;
+            }
+        }
+        
+        return make_pair(links, output);
+    }
+};
