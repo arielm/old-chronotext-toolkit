6cd20cbcdde934ad4b52a137d152571d9baf0f27
arielm <ariel@chronotext.org>
2012-08-01 17:23:10 +0300

    COSMETICS

--- Templates/SketchTemplate/src/Sketch.h
+++ Templates/SketchTemplate/src/Sketch.h
@@ -13,15 +13,15 @@ class Sketch : public CinderSketch
 public:
     Sketch(void *context) : CinderSketch(context) {}
 
-	void setup();
-	void shutdown();
-	void resize(ci::app::ResizeEvent event);
-	
-	void start(int flags);
-	void stop(int flags);
-	
-	void update();
-	void draw();
+    void setup();
+    void shutdown();
+    void resize(ci::app::ResizeEvent event);
+
+    void start(int flags);
+    void stop(int flags);
+
+    void update();
+    void draw();
 
 #if defined( CINDER_COCOA_TOUCH )
     void accelerated(ci::app::AccelEvent event);
