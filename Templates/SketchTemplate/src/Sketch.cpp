e22dc853ee66a3f19961222f0c13257da1e55646
arielm <ariel@chronotext.org>
2012-08-01 17:22:27 +0300

    COSMETICS

--- Templates/SketchTemplate/src/Sketch.cpp
+++ Templates/SketchTemplate/src/Sketch.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 void Sketch::setup()
 {
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glEnable(GL_BLEND);
-	
-	glDisable(GL_DEPTH_TEST);
-	glDepthMask(GL_FALSE);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+
+    glDisable(GL_DEPTH_TEST);
+    glDepthMask(GL_FALSE);
 }
 
 void Sketch::shutdown()
@@ -21,20 +21,20 @@ void Sketch::shutdown()
 void Sketch::start(int flags)
 {
 #if defined( CINDER_COCOA_TOUCH )
-	enableAccelerometer(30);
+    enableAccelerometer(30);
 #endif
 }
 
 void Sketch::stop(int flags)
 {
 #if defined( CINDER_COCOA_TOUCH )
-	disableAccelerometer();
+    disableAccelerometer();
 #endif
 }
 
 void Sketch::resize(ResizeEvent event)
 {
-	gl::setMatricesWindow(event.getSize(), true);
+    gl::setMatricesWindow(event.getSize(), true);
 }
 
 void Sketch::update()
@@ -43,10 +43,10 @@ void Sketch::update()
 void Sketch::draw()
 {
     glClearColor(1, 0, 0, 1); 
-	glClear(GL_COLOR_BUFFER_BIT); 
+    glClear(GL_COLOR_BUFFER_BIT); 
 	
     glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+    glLoadIdentity();
 }
 
 // ======================================== ACCELEROMETER ========================================
