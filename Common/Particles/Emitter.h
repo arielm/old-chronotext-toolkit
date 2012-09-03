d8c51fc3d5455bd3a5377c20732004f84773dda8
Ariel Malka <ariel@chronotext.org>
2012-09-03 10:55:25 +0300

    INTRODUCING Random.h, HANDY FOR ALLOWING TO COMPILE OBJECTIVE-C++ WITH GCC WHILE USING cinder/Rand.h

--- Common/Particles/Emitter.h
+++ Common/Particles/Emitter.h
@@ -5,21 +5,7 @@
 #include "TextureAtlas.h"
 #include "EmitterParams.h"
 #include "EmitterController.h"
-
-#include "cinder/Rand.h"
-
-/*
- * THE FOLLOWING IS NECESSARY IN ORDER TO
- * KEEP-ON COMPILING OBJECTIVE-C++ WITH GCC
- */
-
-#ifndef Nil
-#define Nil __DARWIN_NULL
-#endif
-
-#ifndef nil
-#define nil __DARWIN_NULL
-#endif
+#include "Random.h"
 
 namespace chronotext
 {
---
+++ Toolkit/Maths/Random.h
@@ -0,0 +1,14 @@
+#include "cinder/Rand.h"
+
+/*
+ * THE FOLLOWING IS NECESSARY IN ORDER TO
+ * KEEP-ON COMPILING OBJECTIVE-C++ WITH GCC
+ */
+
+#ifndef Nil
+#define Nil __DARWIN_NULL
+#endif
+
+#ifndef nil
+#define nil __DARWIN_NULL
+#endif
