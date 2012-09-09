03b44910363f2e15bb02998c222bcf08aaed1a4e
Ariel Malka <ariel@chronotext.org>
2012-09-09 14:57:35 +0300

    MessageQueue: TEMPORARY WORKAROUND TO DISABLE THREAD-LOCKING (WHICH CAN MAKE PROBLEMS ON IOS...)

--- Toolkit/OS/MessageQueue.cpp
+++ Toolkit/OS/MessageQueue.cpp
@@ -6,7 +6,9 @@ Message MessageQueue::nextMessage()
 {
     Message message;
     
+#ifndef MESSAGE_QUEUE_LOCK_FREE
     queueMutex.lock();
+#endif
     
     if (!queue.empty())
     {
@@ -14,16 +16,24 @@ Message MessageQueue::nextMessage()
         queue.pop();
     }
     
+#ifndef MESSAGE_QUEUE_LOCK_FREE
     queueMutex.unlock();
-    
+#endif
+
     return message;
 }
 
 bool MessageQueue::enqueueMessage(const Message &message)
 {
+#ifndef MESSAGE_QUEUE_LOCK_FREE
     queueMutex.lock();
+#endif
+    
     queue.push(message);
+    
+#ifndef MESSAGE_QUEUE_LOCK_FREE
     queueMutex.unlock();
+#endif
     
     return true;
 }
