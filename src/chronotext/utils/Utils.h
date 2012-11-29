ffe4ed9660c9c7849c805dae70bc31feff95ebff
arielm <ariel@chronotext.org>
2012-11-29 16:19:42 +0200

    Utils: ROLLING-BACK "DLOG FOR ANDROID AND MSW", AS IT IS LESS TRIVIAL THAN IT SOUNDS

--- src/chronotext/utils/Utils.h
+++ src/chronotext/utils/Utils.h
@@ -8,23 +8,11 @@
 #include <iostream>
 
 /*
- * LOGGING
+ * DEBUG LOGGING (FOR COCOA ONLY)
  */
 
-#if defined(CINDER_MSW)
-#include "cinder/msw/OutputDebugStringStream.h"
-static cinder::msw::dostream *gOutpuStream = new cinder::msw::dostream;
-#elif defined(CINDER_ANDROID)
-#include "cinder/android/LogStream.h"
-static cinder::android::dostream *gOuputStream = new cinder::android::dostream;
-#endif
-
 #if defined(DEBUG) || defined(FORCE_LOG)
-    #if defined(CINDER_COCOA)
-    #define DLOG(x) std::cout << x << std::endl
-    #else
-    #define DLOG(x) gOuputStream << x << std::endl
-    #endif
+#define DLOG(x) std::cout << x << std::endl
 #else
 #define DLOG(x)
 #endif
